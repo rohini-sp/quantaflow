df1896b3fda762da281c2cf5be9a02acc0b9be4e
Rohini Pedamkar <138889836+rohini-sp@users.noreply.github.com>
2025-07-26 20:54:00 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // 👈 Required for using the /app directory
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
